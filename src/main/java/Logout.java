77fce90dca7acf0578ad5aca26cb28926ac1bf62
Joshua Asiddao <115857349+JDAsiddao@users.noreply.github.com>
2023-05-19 00:43:31 +0800

    Update Logout.java

--- src/main/java/Logout.java
+++ src/main/java/Logout.java
@@ -1,13 +1,8 @@
 
-
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 import java.io.IOException;
-import jakarta.servlet.http.HttpSession;
+
 
 public class Logout extends HttpServlet {
 	
