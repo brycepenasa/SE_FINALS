f34081a2f60e4bbc01a69c5225c33aed30b3dc4a
Joshua Asiddao <115857349+JDAsiddao@users.noreply.github.com>
2023-05-19 00:44:13 +0800

    Update order.java

--- src/main/java/newp/order.java
+++ src/main/java/newp/order.java
@@ -1,14 +1,8 @@
 package newp;
 
-import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 import java.sql.*;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
 
 @WebServlet("/order")
