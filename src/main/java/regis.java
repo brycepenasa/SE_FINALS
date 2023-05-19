b78743a461d99c1c72c90ac8fcd24fe7c0cbf969
Joshua Asiddao <115857349+JDAsiddao@users.noreply.github.com>
2023-05-19 20:18:37 +0800

    Update regis.java

--- src/main/java/regis.java
+++ src/main/java/regis.java
@@ -1,5 +1,10 @@
-import jakarta.servlet.*;
-import javax.servlet.annotation.WebServlet;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
