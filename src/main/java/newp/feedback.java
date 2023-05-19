56d16efbd19ef936bd7510ff5eb67435a4b4a852
Joshua Asiddao <115857349+JDAsiddao@users.noreply.github.com>
2023-05-19 20:00:12 +0800

    Update feedback.java

--- src/main/java/newp/feedback.java
+++ src/main/java/newp/feedback.java
@@ -38,7 +38,7 @@ public class feedback extends HttpServlet {
 	   
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sefinals?useSSL=false", "root", "admin");
+			con = DriverManager.getConnection("jdbc:mysql://soundsdreamy.mysql.database.azure.com:3306/", "admindreamy", "@Soundsdreamy10");
 			
 			pstmt3 = con.prepareStatement(query4);
 			pstmt3.setString(1, uname);
