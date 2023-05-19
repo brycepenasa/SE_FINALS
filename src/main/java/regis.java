c6aaf5c9dc857f1f8b1b5006a3d86b9520323646
Joshua Asiddao <115857349+JDAsiddao@users.noreply.github.com>
2023-05-19 19:59:34 +0800

    Update regis.java

--- src/main/java/regis.java
+++ src/main/java/regis.java
@@ -38,7 +38,7 @@ public class regis extends HttpServlet {
 	    ResultSet rs = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sefinals?useSSL=false", "root", "admin");
+			con = DriverManager.getConnection("jdbc:mysql://soundsdreamy.mysql.database.azure.com:3306/", "admindreamy", "@Soundsdreamy10");
 			pstmt = con.prepareStatement(query);
 	         pstmt.setString(1, uname);
 	         pstmt.setString(2, email);
