f759625b30e323ce72e43f85e6edcb8572affaa6
Joshua Asiddao <115857349+JDAsiddao@users.noreply.github.com>
2023-05-19 20:22:42 +0800

    Update LogDatabase.java

--- src/main/java/newp/LogDatabase.java
+++ src/main/java/newp/LogDatabase.java
@@ -10,7 +10,7 @@ public class LogDatabase {
 		boolean status = false;
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		try (Connection connection = DriverManager									
-				.getConnection("jdbc:mysql://localhost:3306/sefinals?useSSL=false", "root", "admin");
+				.getConnection("jdbc:mysql://soundsdreamy.mysql.database.azure.com:3306/sefinals?useSSL=true", "admindreamy", "@Soundsdreamy10");
 																	
 				PreparedStatement ps = connection
 						.prepareStatement("SELECT * FROM users WHERE uname = ? and pass = ? ")) {
